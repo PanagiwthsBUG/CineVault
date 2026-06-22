8cdb440950a5feb202b7f1108345f82f825632a8
Jast <panosbudev@gmail.com>
2026-06-22 08:09:46 +0300

    Implement JWT authentication

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const pool = require("../db/db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   try {
@@ -57,6 +58,83 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+
+    if (!email || !password) {
+      return res.status(400).json({
+        error: "Email and password are required",
+      });
+    }
+
+
+    const result = await pool.query(
+      `
+      SELECT *
+      FROM users
+      WHERE email = $1
+      `,
+      [email]
+    );
+
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({
+        error: "Invalid credentials",
+      });
+    }
+
+
+    const user = result.rows[0];
+
+
+    const passwordMatch = await bcrypt.compare(
+      password,
+      user.password_hash
+    );
+
+
+    if (!passwordMatch) {
+      return res.status(401).json({
+        error: "Invalid credentials",
+      });
+    }
+
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+
+    res.json({
+      token,
+    });
+
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      error: "Internal server error",
+    });
+
+  }
+};
+
+
 module.exports = {
   registerUser,
+  loginUser,
 };
